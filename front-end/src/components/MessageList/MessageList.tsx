b9ade964aa4b7f9e7d8adc31847dee032b615de2
Euan Mendoza <bebopbamf@effectfree.dev>
2021-08-29 01:59:45 +1000

    better fetcher

--- front-end/src/components/MessageList/MessageList.tsx
+++ front-end/src/components/MessageList/MessageList.tsx
@@ -9,10 +9,6 @@ import { ChatData } from "data/chat";
 
 const APIUrl = 'https://7a9ot7v4s0.execute-api.ap-southeast-2.amazonaws.com/api';
 
-export const getData = (path: string) => {
-    return fetch(APIUrl + path)
-        .then(res => res.json())
-};
 interface Props {
   children?: ComponentChildren;
 }
@@ -24,36 +20,7 @@ const FailPage = () => (
 );
 
 const MessageList: FunctionalComponent<Props> = (props: Props) => {
-  const { data, error } = useSWR('/chats', getData);
-
-    /*
-  const stubUser: User = {
-    guid: '1111111-111111-11111-111',
-    name: 'Matt',
-    emoji: '🍆',
-    location: {
-      locale: 'en_AU.UTF-8',
-      name: 'Sydney, Australia',
-      emoji: '🇦🇺'
-    },
-    lastPlayed: {
-      uri: '',
-      title: 'The Lazy Song',
-      artist: 'Bruno Mars',
-      explicit: false,
-      duration: 10323,
-      image: ''
-    },
-    favouriteSongs: {
-      first: undefined,
-      second: undefined,
-      third: undefined,
-    },
-    followingUser: ['123232132'],
-  };
-  */
-
-  // const fetchUserDataStub = map(_ => stubUser)(Array(10).fill(0));
+  const { data, error } = useSWR('/chats');
 
   const transformSongData = map((data: ChatData) => (<MessageListItem chat={data} />));
 
--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -2,20 +2,31 @@ import { UserProvider } from "./context/UserContext";
 import { h, render } from "preact";
 import { Router } from "preact-router";
 import { Home, Messages, Profile, SignUp } from "./routes";
+import { SWRConfig } from 'swr';
 
 import "./style.css";
+
+const baseUrl = 'https://7a9ot7v4s0.execute-api.ap-southeast-2.amazonaws.com/api';
+
+const fetcher = (uri: string, opts: object) => fetch(baseUrl + uri, opts).then(res => res.json());
 
 const App = () => {
   return (
     <div className="flex flex-col w-screen h-screen bg-gray-50">
-      <UserProvider>
-        <Router>
-          <Home path="/" />
-          <Messages path="/messages" />
-          <SignUp path="/signup" />
-          <Profile path="/profile" isEditable={true} />
-        </Router>
-      </UserProvider>
+        <SWRConfig
+          value={{
+            fetcher: fetcher
+          }}
+        >
+        <UserProvider>
+          <Router>
+            <Home path="/" />
+            <Messages path="/messages" />
+            <SignUp path="/signup" />
+            <Profile path="/profile" isEditable={true} />
+          </Router>
+        </UserProvider>
+      </SWRConfig>
     </div>
   );
 };
